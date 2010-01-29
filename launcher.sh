3f7b85eda8500474f3c361f7af25fc5f3cc120cf
Joshua Holbrook <josh.holbrook@gmail.com>
2010-01-29 14:05:18 -0900

    same deal with launcher?

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 #This serves as a "launcher" for kssh.sh, which
 #needs to run inside kshell to work properly.
-
-SHELLUSR=$1
-TEHYUORPC=$2
-
 #substitute in whatever terminal you like.
-gnome-terminal -e "kshell -c './kssh.sh $SHELLUSR $TEHYUORPC'" &
+
+gnome-terminal -e "kshell -c './kssh.sh'" &
 
