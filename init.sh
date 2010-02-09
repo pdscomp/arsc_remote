bf5ca1d12b934f527c68e0840323315e9cf39153
Joshua Holbrook <josh.holbrook@gmail.com>
2010-02-09 11:05:37 -0900

    Found a way to get around aliases not being inherited by kshell

--- init.sh
+++ init.sh
@@ -1,1 +1,17 @@
-kshell && . ./config/kshell_profile
+#!/usr/bin/env bash
+
+#Some handy variables
+SHELLUSR=`cat ./config/username`
+#TEHYUORPC=`cat ./config/tehyuorpc`
+DOMAIN='arsc.edu'
+CONFIGFILE='./config/ssh_config'
+export SHELLUSR
+#export TEHYUORPC
+export DOMAIN
+export CONFIGFILE
+
+#adds special krb5 and ossh paths
+PATH=./scripts:$PATH
+export PATH
+
+kshell
